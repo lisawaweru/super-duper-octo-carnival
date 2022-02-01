ee897f01f754887d770a6e288db99f91af00e368
llisawaweru <lisaannewaweru@gmail.com>
2022-02-01 14:56:50 -0600

    first version

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 function main() {
-    //              0123456789ABCDE
-    const myFirstName = 'Lisa';
-    const myLastName ='Waweru';
-    const mySplit = [myFirstName, myLastName];
-    const myMidlleName = 'Mumbi';
-    console.log(myFirstName + " "+ myLastName)
-    console.log(mySplit)
-    console.log(myMidlleName)
-    console.log(myFirstName.toLowerCase())
-    console.log(myLastName.toUpperCase())
-    console.log(mySplit.reverse().join(' '));
-    console.log(myFirstName.substring(0,1));
-    console.log(myLastName.substring(0,3));
-    console.log(myFirstName[0] + myLastName[0]);
+    const myMessage = "Hello world!";
+    let result = '';
 
+for (let i = 0; i < myMessage.length; i +=1) {
+    const char = myMessage[i];
+
+    result += char.concat(' ');
 }
 
-main ();
+console.log(result);
+}  
+
+main();
+
+
+
